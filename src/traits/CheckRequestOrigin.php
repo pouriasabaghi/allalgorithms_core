f15883b1b1a5037d5b433794e9cddcbd8e9156fc
Pouria Sabaghi <pouria.782@gmail.com>
2024-09-25 15:38:02 +0330

    refactor: refactor CheckRequestOrigin trait for having cleaner code

--- src/traits/CheckRequestOrigin.php
+++ src/traits/CheckRequestOrigin.php
@@ -7,27 +7,15 @@ trait CheckRequestOrigin
 {
     public function isOriginValid()
     {
-        if (config('app_mode') === 'prod') {
-            $allowed = config('allowed_prod_origin')[0] === "*"
-                ? true
-                : config('allowed_prod_origin');
-        } else {
-            $allowed = config('allowed_local_origin')[0] === "*"
-                ? true
-                : config('allowed_local_origin');
-        }
+        $allowedOrigins = config(config('app_mode') === 'prod' ? 'allowed_prod_origin' : 'allowed_local_origin');
+    
+        if ($allowedOrigins[0] === "*") 
+            return true;
 
-        if ($allowed === true)
-            return $allowed;
-
-
-        if (is_array($allowed))
-            if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed))
-                return true;
-            else
-                return false;
-
+        if (is_array($allowedOrigins) && isset($_SERVER['HTTP_ORIGIN'])) 
+            return in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins);
+        
         return false;
-
     }
+    
 }
